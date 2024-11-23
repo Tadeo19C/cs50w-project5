dbfbd3b60f2583b60cc99cbe6fde353209d73c60
Tadeo19C <136045991+Tadeo19C@users.noreply.github.com>
2024-11-23 08:08:14 -0600

    Update settings.py

--- StudyChatRoom/settings.py
+++ StudyChatRoom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pv4x+k2rj%l3wzynt!s=!hlx3jdz$+17@8hig9jvgy-cr$!w66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
